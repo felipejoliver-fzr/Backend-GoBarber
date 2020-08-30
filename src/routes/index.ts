df79a2cb2583b81c95a87bd554131e2e8cd39739
Felipe Oliveira <felipe2.oliveira@gmail.com>
2020-08-29 23:32:45 -0300

    cadastro do user e relacionamento com appointment

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import appoitmentsRoutes from "./appointments.routes";
+import usersRoutes from "./users.routes";
 
 const routes = Router();
 
 routes.use('/appointments', appoitmentsRoutes);
+routes.use('/users', usersRoutes);
 
 export default routes;
