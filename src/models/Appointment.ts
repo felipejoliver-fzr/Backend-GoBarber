50f94b4155852b2fc23cf70b56c6a466ccfe73a6
Felipe Oliveira <felipe2.oliveira@gmail.com>
2020-08-29 22:09:23 -0300

    conexão com o banco e crud appointment

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,18 +1,16 @@
-import { uuid } from "uuidv4"
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity('appointments')
 class Appointment {
+    @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @Column()
     provider: string;
 
+    @Column('timestamp with time zone')
     date: Date;
 
-    //o Omit server para omitir atributos em que não é desejado na entradad e algum objeto
-    constructor({ provider, date }: Omit<Appointment, 'id'>){
-        this.id = uuid();
-        this.provider = provider;
-        this.date = date;
-    }
 }
 
 export default Appointment;
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -2,7 +2,7 @@ import { startOfHour } from 'date-fns';
 
 import Appointment from "../models/Appointment";
 import AppointmentsRepository from "../repositories/AppointmentsRepository";
-
+import { getCustomRepository } from "typeorm";
 interface Request {
     date: Date;
     provider: string;
@@ -10,25 +10,24 @@ interface Request {
 
 class CreateAppointmentService {
 
-    private appointmentsRepository: AppointmentsRepository;
+    public async execute({ date, provider }: Request): Promise<Appointment> {
 
-    constructor(appointmentsRepository: AppointmentsRepository) {
-        this.appointmentsRepository = appointmentsRepository;
-    }
+        const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-    public execute({ date, provider }: Request): Appointment {
         const appointmentDate = startOfHour(date);
 
-        const findAppointmentInSameDate = this.appointmentsRepository.findByDate(appointmentDate);
+        const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
 
         if (findAppointmentInSameDate) {
             throw Error('This appointment is already booked');
         }
 
-        const appointment = this.appointmentsRepository.create({
+        const appointment = appointmentsRepository.create({
             provider,
             date: appointmentDate
         });
+
+        await appointmentsRepository.save(appointment);
 
         return appointment;
     }
