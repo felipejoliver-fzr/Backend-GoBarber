c1ecb05f34294f2c35d47780e443190ff92d2feb
Felipe Oliveira <felipe2.oliveira@gmail.com>
2020-08-30 01:57:05 -0300

    tratativa de exceptions

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -12,36 +12,31 @@ const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
 
-    try {
-        const { name, email, password } = request.body;
+    const { name, email, password } = request.body;
 
-        const createUser = new CreateUserService();
+    const createUser = new CreateUserService();
 
-        const user = await createUser.execute({name, email, password: password});
+    const user = await createUser.execute({ name, email, password: password });
 
-        delete user.password;
+    delete user.password;
 
-        return response.json(user);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
+    return response.json(user);
+
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-    try{
-        const updateUserAvatar = new UpdateUserAvatarService();
 
-        const user = await updateUserAvatar.execute({
-            user_id: request.user.id,
-            avatarFilename: request.file.filename
-        });
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-        delete user.password;
+    const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename
+    });
 
-        return response.json(user);
-    } catch {
+    delete user.password;
 
-    }
+    return response.json(user);
+
 });
 
 export default usersRouter;
