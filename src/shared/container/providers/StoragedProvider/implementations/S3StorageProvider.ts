239128083f4e664e4f0cc307bb1c70b53928e2db
Felipe Oliveira <felipeoliveira@Felipes-MacBook-Pro.local>
2020-11-01 17:46:46 -0300

    Upload de arquivos utilizando o Amazon S3

---
+++ src/shared/container/providers/StoragedProvider/implementations/S3StorageProvider.ts
@@ -0,0 +1,62 @@
+import fs from "fs";
+import path from "path";
+import mime from "mime";
+import aws, { S3 } from "aws-sdk";
+
+import uploadConfig from "@config/upload";
+import IStorageProvider from "../models/IStorageProvider";
+
+class DiskStorageProvider implements IStorageProvider {
+    private client: S3;
+
+    constructor() {
+        this.client = new aws.S3({
+            region: "us-east-1",
+        });
+    }
+
+    public async saveFile(file: string): Promise<string> {
+        const originalPath = path.resolve(uploadConfig.tmpFolder, file);
+
+        const ContentType = mime.getType(originalPath);
+
+        if (!ContentType) {
+            throw new Error('File not found');
+        }
+
+        const fileContent = await fs.promises.readFile(originalPath);
+
+        // O encoding é preciso para arquivos do tipo texto
+        // const fileContent = await fs.promises.readFile(originalPath, {
+        //     encoding: "utf-8",
+        // });
+
+        await this.client
+            .putObject({
+                Bucket: uploadConfig.config.aws.bucket,
+                //KEY: nome do arquivo
+                Key: file,
+                //ACL: qual é o tipo de permissão para o arquivo
+                ACL: "public-read",
+                //Body: o arquivo em si
+                Body: fileContent,
+                ContentType
+            })
+            .promise();
+
+        await fs.promises.unlink(originalPath);
+
+        return file;
+    }
+
+    public async deleteFile(file: string): Promise<void> {
+        await this.client
+            .deleteObject({
+                Bucket: uploadConfig.config.aws.bucket,
+                Key: file,
+            })
+            .promise();
+    }
+}
+
+export default DiskStorageProvider;
