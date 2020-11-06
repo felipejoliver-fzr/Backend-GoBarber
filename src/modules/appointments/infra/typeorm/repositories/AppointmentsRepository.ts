5730e598334b6fdd843fbdcfc285333a37a2afb9
Felipe Oliveira <felipeoliveira@Felipes-MacBook-Pro.local>
2020-11-05 22:15:00 -0300

    correções

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -51,21 +51,21 @@ class AppointmentsRepository implements IAppointmentsRepository {
         year,
     }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
         //padStart -> se a string não conter 2 caracteres, inserir 0 à esquerda
-        const parsedDay = String(day).padStart(2, "0");
-        const parsedMonth = String(month).padStart(2, "0");
+        const parsedDay = String(day).padStart(2, '0');
+    const parsedMonth = String(month).padStart(2, '0');
 
-        const appointments = await this.ormRepository.find({
-            where: {
-                provider_id,
-                date: Raw(
-                    (dateFieldName) =>
-                        `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`
-                ),
-            },
-            relations: ['user']
-        });
+    const appointments = await this.ormRepository.find({
+      where: {
+        provider_id,
+        date: Raw(
+          dateFieldName =>
+            `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
+        ),
+      },
+      relations: ['user'],
+    });
 
-        return appointments;
+    return appointments;
     }
 
     public async create({
--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -32,6 +32,7 @@ class ListProviderAppointmentsService {
         let appointments = await this.cacheProvider.recover<Appointment[]>(
             cacheKey
         );
+
         if (!appointments) {
             appointments = await this.appointmentsRepository.findAllInDayFromProvider(
                 {
